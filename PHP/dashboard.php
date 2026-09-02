2ff3f03cf1cd9ed8e378c053b658bc1bfd150c7a
Aaron <aaron@example.com>
2026-09-02 15:08:48 +0300

    update

--- PHP/dashboard.php
+++ PHP/dashboard.php
@@ -62,8 +62,8 @@ function fmtNum($val) {
     :root {
         --crdb-green: #357600;
         --crdb-gold: #f2a900;
-        --bg: #f5f7fa;
-        --card-bg: #ffffff;
+        --bg: #d4e211e7;
+        --card-bg: #12c53f;
         --border: #e1e5eb;
         --text: #1a1a1a;
         --text-muted: #5c6470;
