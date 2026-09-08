28be8b77b7ad604e5308208e8f14c3c56a0798e7
Aaronsiara <siarraaaron@gmail.com>
2026-09-08 11:06:14 +0300

    return to original

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,30 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-// 1. Make sure this import is at the top of your controller file
-use Illuminate\Support\Facades\Process; 
-
-class SegmentationController extends Controller
+abstract class Controller
 {
-    public function runSegmentation()
-    {
-        // 2. Put Option 1 right here inside your method
-        $result = Process::withEnvironment([
-            'USERPROFILE' => 'C:\xampp\tmp',
-            'MPLCONFIGDIR' => 'C:\xampp\tmp',
-        ])->run([
-            'E:/anaconda/python.exe',
-            'C:\xampp\htdocs\crdb-segmentation\scripts\segment_any_data.py',
-            '--input', 'C:\xampp\htdocs\crdb-segmentation\storage\app/private/segmentation/20260908_064754_Bciu1a/input.csv',
-            '--output-dir', 'C:\xampp\htdocs\crdb-segmentation\storage\app/private/segmentation/20260908_064754_Bciu1a',
-            '--k', '10'
-        ]);
-
-        if ($result->successful()) {
-            return response()->json(['message' => 'Success!', 'output' => $result->output()]);
-        }
-
-        return response()->json(['error' => $result->errorOutput()], 500);
-    }
+    //
 }
